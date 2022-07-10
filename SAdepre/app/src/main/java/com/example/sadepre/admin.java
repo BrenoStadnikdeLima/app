22eefb41c9a5705d99b88186f333a5ca423e1e19
BrenoStadnikdeLima <brenostadnikdelima123@gmail.com>
2022-07-10 19:55:23 -0300

    up
    teste

---
+++ SAdepre/app/src/main/java/com/example/sadepre/admin.java
@@ -0,0 +1,14 @@
+package com.example.sadepre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.telaadmin1);
+    }
+}
---
+++ SAdepre/app/src/main/java/com/example/sadepre/regiaoadmin.java
@@ -0,0 +1,14 @@
+package com.example.sadepre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class regiaoadmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.telaadmin2);
+    }
+}
